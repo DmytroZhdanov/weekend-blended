26a8ecf00efbda059b76080e2bc59c171fcc2a9b
Maryna Skorokhod <m.bilous13@ukr.net>
2023-03-18 13:04:32 +0200

    adding task2

--- week-1.js
+++ week-1.js
@@ -8,18 +8,35 @@
 
 //  *?      70 === 01:10
 
-function normilizeTimeFormat() {
-  const userData = +prompt("Enter required minutes");
-  
-  if (isNaN(userData)) {
-    alert("Your data is not valid");
-  }
-  
-  const hours = String(Math.floor(userData / 60)).padStart(2, 0);
-  const minutes = String(userData % 60).padStart(2, 0);
+// function normilizeTimeFormat() {
+//   const userData = +prompt("Enter required minutes");
 
-  console.log(`Your time is ${hours}:${minutes}`)
-  alert(`Your time is ${hours}:${minutes}`);
+//   if (isNaN(userData)) {
+//     alert("Your data is not valid");
+//   }
+
+//   const hours = String(Math.floor(userData / 60)).padStart(2, 0);
+//   const minutes = String(userData % 60).padStart(2, 0);
+
+//   console.log(`Your time is ${hours}:${minutes}`)
+//   alert(`Your time is ${hours}:${minutes}`);
+// }
+
+// normilizeTimeFormat();
+
+// TASK2
+// const arr = ['BEST', 'the', 'foo', 'is', 'JS'];  Напишіть функцію яка отримуєм масив вирізає фу та розвертає масис і створює рядок
+
+const arr = ["BEST", "the", "foo", "is", "JS"];
+function updateUserInfo(array, str) {
+  const revArr = [...array].reverse();
+  console.log(revArr);
+  const idxStr = revArr.indexOf(str);
+  console.log(idxStr);
+  const delEl = revArr.splice(idxStr, 1);
+  console.log(delEl);
+  const resultStr = revArr.join(" ");
+  alert(resultStr);
+  return resultStr;
 }
-
-normilizeTimeFormat();
+console.log(updateUserInfo(arr, "foo"));
