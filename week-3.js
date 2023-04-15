dbb16a97a56d04b741d45687e272fc3f8315f928
DmytroZhdanov <119117057+DmytroZhdanov@users.noreply.github.com>
2023-04-15 12:04:36 +0300

    Comments task 1

--- week-3.js
+++ week-3.js
@@ -6,24 +6,24 @@
 
 // ("^[А-Яа-яЁёЇїІіЄєҐґ']+$");
 
-const generateValidator = (expression) => {
-  return (value) => {
-    return expression.test(value);
-  };
-};
+// const generateValidator = (expression) => {
+//   return (value) => {
+//     return expression.test(value);
+//   };
+// };
 
-const nameValidator = generateValidator(/^[a-zA-Zа-яА-Я'][a-zA-Zа-яА-Я-' ]+[a-zA-Zа-яА-Я']?$/u);
-const phoneValidator = generateValidator(/^(\s*)?(\+)?([- _():=+]?\d[- _():=+]?){10,14}(\s*)?$/);
-const emaildValidator = generateValidator(
-  /^([A-Za-z0-9_\-\.])+\@([A-Za-z0-9_\-\.])+\.([A-Za-z]{2,4})$/
-);
+// const nameValidator = generateValidator(/^[a-zA-Zа-яА-Я'][a-zA-Zа-яА-Я-' ]+[a-zA-Zа-яА-Я']?$/u);
+// const phoneValidator = generateValidator(/^(\s*)?(\+)?([- _():=+]?\d[- _():=+]?){10,14}(\s*)?$/);
+// const emaildValidator = generateValidator(
+//   /^([A-Za-z0-9_\-\.])+\@([A-Za-z0-9_\-\.])+\.([A-Za-z]{2,4})$/
+// );
 
 
-console.log(nameValidator('Poly'));
-console.log(nameValidator('5467'));
+// console.log(nameValidator('Poly'));
+// console.log(nameValidator('5467'));
 
-console.log(phoneValidator('Mango'));
-console.log(phoneValidator('+380502388238'));
+// console.log(phoneValidator('Mango'));
+// console.log(phoneValidator('+380502388238'));
 
-console.log(emaildValidator('krosh@org.ua'));
-console.log(emaildValidator('aJax'));
+// console.log(emaildValidator('krosh@org.ua'));
+// console.log(emaildValidator('aJax'));
